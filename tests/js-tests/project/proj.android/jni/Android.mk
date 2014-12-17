0e78939579945148a2ec0bc41fb8ed74f33195dd
zhangbin <zhangbin@cocos2d-x.org>
2014-12-17 18:23:50 +0800

    Run js-tests succeed on android.

--- tests/js-tests/project/proj.android/jni/Android.mk
+++ tests/js-tests/project/proj.android/jni/Android.mk
@@ -19,5 +19,5 @@ LOCAL_EXPORT_CFLAGS := -DCOCOS2D_DEBUG=1
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,bindings) 
-$(call import-module,cocos2d-x/plugin/jsbindings)
+$(call import-module,scripting/js-bindings) 
+$(call import-module,plugin/jsbindings)
